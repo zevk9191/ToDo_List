a599ad90ab1495af1da44043f029525732d4e66e
Zakhar Bihun <12zahar34@gmail.com>
2022-10-10 16:44:34 +0300

    add new features

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const form = document.body.querySelector('form');
-const taskList = document.getElementById('taskList');
+const taskList = document.querySelector('#taskList');
 const textInput = document.querySelector('.inputText');
+const btnClearList = document.querySelector('#btnClearList');
+const btnClearCompleted = document.querySelector('#btnClearCompleted');
+const selectTypeTask = document.querySelector('#selectTypeTask');
+const outNumberTasks = document.querySelector('#outNumberTasks');
 const sequenceNumberEnter = 13;
 
 form.addEventListener("submit", createTask);
 
 function createTask(event) {
     event.preventDefault();
-    const div = document.createElement("div");
+    const li = document.createElement("li");
     const divText = document.createElement("div");
     const input = document.createElement("input");
     const buttonDelete = document.createElement("button");
@@ -18,20 +22,21 @@ function createTask(event) {
     divText.className = 'taskText';
     buttonEdit.textContent = 'Edit';
     buttonDelete.textContent = 'Delete';
-    div.className = 'tasks';
+    li.className = 'tasks';
+    li.setAttribute("data-status", "active")
     if (textInput.value === '') {
         return
     }
     divText.textContent = textInput.value
     textInput.value = '';
-    div.append(input);
-    div.append(divText);
-    div.append(buttonEdit);
-    div.append(buttonDelete);
-    taskList.append(div);
+    li.append(input);
+    li.append(divText);
+    li.append(buttonEdit);
+    li.append(buttonDelete);
+    taskList.append(li);
     taskList.style.visibility = 'visible'
     buttonDelete.addEventListener("click", () => {
-        div.remove()
+        li.remove()
     });
     buttonEdit.addEventListener("click", () => {
         divText.setAttribute('contenteditable', 'true');
@@ -48,7 +53,59 @@ function createTask(event) {
             divText.blur();
         }
     });
+
     input.addEventListener("click", function () {
-        divText.classList.toggle('active');
+        divText.classList.toggle("active");
+        // outNumberTasks.textContent = document.getElementsByClassName("active").length;
+        // li.classList.toggle("active");
+        if (li.dataset.status === "active") {
+            li.dataset.status = "complete";
+        } else {
+            li.dataset.status = "active";
+        }
     });
 }
+
+btnClearList.addEventListener("click", () => {
+    taskList.innerHTML = '';
+});
+
+btnClearCompleted.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        if (taskList.children[i].dataset.status === "complete") {
+            taskList.children[i].remove()
+        }
+    }
+});
+
+selectTypeTask.addEventListener("change", () => {
+    switch (selectTypeTask.selectedIndex) {
+        case 0 :
+            for (let i = taskList.children.length - 1; i >= 0; i--) {
+                taskList.children[i].style.display = "flex"
+            }
+            break;
+        case 1 :
+            for (let i = taskList.children.length - 1; i >= 0; i--) {
+                if (taskList.children[i].dataset.status === "active") {
+                    taskList.children[i].style.display = "flex"
+                } else {
+                    taskList.children[i].style.display = "none"
+                }
+            }
+            break;
+        case 2 :
+            for (let i = taskList.children.length - 1; i >= 0; i--) {
+                if (taskList.children[i].dataset.status === "complete") {
+                    taskList.children[i].style.display = "flex"
+                } else {
+                    taskList.children[i].style.display = "none"
+                }
+            }
+            break;
+        default:
+            for (let i = taskList.children.length - 1; i >= 0; i--) {
+                taskList.children[i].style.display = "flex"
+            }
+    }
+});
