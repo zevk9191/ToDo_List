d43aaebcfb92194baf14d292b0a7e311b6e963ed
Zakhar Bihun <12zahar34@gmail.com>
2022-10-11 14:09:08 +0300

    add new features

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const btnClearList = document.querySelector('#btnClearList');
 const btnClearCompleted = document.querySelector('#btnClearCompleted');
 const selectTypeTask = document.querySelector('#selectTypeTask');
 const outNumberTasks = document.querySelector('#outNumberTasks');
-const sequenceNumberEnter = 13;
+const SEQUENCENUMBERENTER = 13;
 
 form.addEventListener("submit", createTask);
 
@@ -13,10 +13,8 @@ function createTask(event) {
     event.preventDefault();
     const li = document.createElement("li");
     const divText = document.createElement("div");
-    const input = document.createElement("input");
     const buttonDelete = document.createElement("button");
     const buttonEdit = document.createElement("button");
-    input.setAttribute('type', 'checkbox');
     buttonEdit.className = 'taskButton btnEdit';
     buttonDelete.className = 'taskButton btnDelete';
     divText.className = 'taskText';
@@ -29,39 +27,46 @@ function createTask(event) {
     }
     divText.textContent = textInput.value
     textInput.value = '';
-    li.append(input);
     li.append(divText);
     li.append(buttonEdit);
     li.append(buttonDelete);
     taskList.append(li);
-    taskList.style.visibility = 'visible'
+    taskList.style.visibility = "visible";
     buttonDelete.addEventListener("click", () => {
         li.remove()
     });
     buttonEdit.addEventListener("click", () => {
-        divText.setAttribute('contenteditable', 'true');
-        divText.setAttribute('tabindex', '-1');
+        divText.setAttribute("contenteditable", "true");
+        divText.setAttribute("tabindex", "-1");
         divText.focus();
     });
 
-    divText.onblur = () => {
-        divText.setAttribute('contenteditable', 'false')
-    };
-
-    divText.addEventListener('keydown', function (event) {
-        if (event.keyCode === sequenceNumberEnter) {
-            divText.blur();
-        }
+    divText.addEventListener("blur", () => {
+        divText.setAttribute("contenteditable", 'false')
     });
 
-    input.addEventListener("click", function () {
+    divText.addEventListener("click", () => {
         divText.classList.toggle("active");
         // outNumberTasks.textContent = document.getElementsByClassName("active").length;
         // li.classList.toggle("active");
         if (li.dataset.status === "active") {
             li.dataset.status = "complete";
+            // outNumberTasks.textContent += "1";
         } else {
             li.dataset.status = "active";
+        }
+    });
+
+    divText.addEventListener("keydown", function (event) {
+        if (event.keyCode === SEQUENCENUMBERENTER) {
+            divText.blur();
+        }
+    });
+    divText.addEventListener("click", () => {
+        for (let i = taskList.children.length - 1; i >= 0; i--) {
+            if (taskList.children[i].dataset.status === "active") {
+                outNumberTasks.textContent = taskList.children.length;
+            }
         }
     });
 }
