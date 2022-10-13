8ce19550d34a73df7a6b8638e4fb5452cd2c267a
Zakhar Bihun <69964127+zevk9191@users.noreply.github.com>
2022-10-13 17:09:39 +0300

    Add js
    Create html, css, js files, and add basic styles for ToDoList
    add function create task
    
    * delete .idea
    
    * Create folder styles, add ability to create task
    
    * refactor code
    
    * fix problems
    
    * fix problems

---
+++ script.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('form');
+const taskList = document.getElementById('taskList');
+const textInput = document.querySelector('.inputText');
+const SEQUENCE_NUMBER_ENTER = 13;
+
+form.addEventListener("submit", createTask);
+
+function createTask(event) {
+    event.preventDefault();
+    const div = document.createElement("div");
+    const divText = document.createElement("div");
+    const input = document.createElement("input");
+    const buttonDelete = document.createElement("button");
+    const buttonEdit = document.createElement("button");
+    input.setAttribute('type', 'checkbox');
+    buttonEdit.className = 'taskButton btnEdit';
+    buttonDelete.className = 'taskButton btnDelete';
+    divText.className = 'taskText';
+    buttonEdit.textContent = 'Edit';
+    buttonDelete.textContent = 'Delete';
+    div.className = 'tasks';
+    if (!textInput.value) {
+        return;
+    }
+    divText.textContent = textInput.value;
+    textInput.value = '';
+    div.append(input);
+    div.append(divText);
+    div.append(buttonEdit);
+    div.append(buttonDelete);
+    taskList.append(div);
+    taskList.style.visibility = 'visible'
+    buttonDelete.addEventListener("click", () => {
+        div.remove()
+    });
+    buttonEdit.addEventListener("click", () => {
+        divText.setAttribute('contenteditable', 'true');
+        divText.setAttribute('tabindex', '-1');
+        divText.focus();
+    });
+
+    divText.addEventListener("blur", () => {
+        divText.setAttribute('contenteditable', 'false');
+    });
+
+    divText.addEventListener('keydown', function (event) {
+        if (event.keyCode === SEQUENCE_NUMBER_ENTER) {
+            divText.blur();
+        }
+    });
+    input.addEventListener("click", function () {
+        divText.classList.toggle('active');
+    });
+}
