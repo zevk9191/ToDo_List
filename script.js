7678399e921bceda58a03712feb27c2ae1e6b230
Zakhar Bihun <69964127+zevk9191@users.noreply.github.com>
2022-10-15 17:28:17 +0300

    Finally (#3)
    * Create html, css, js files, and add basic styles for ToDoList
    
    * add function create task
    
    * delete .idea
    
    * Create folder styles, add ability to create task
    
    * refactor code
    
    * add new features
    
    * add new features
    
    * add localStorage
    
    * fix script.js
    
    * add new feature to script.js
    
    * fix script.js
    
    * fix script.js
    
    * add and fix final features
    
    * fix features

--- script.js
+++ script.js
@@ -1,54 +1,143 @@
 const form = document.querySelector('form');
-const taskList = document.getElementById('taskList');
+const taskList = document.querySelector('#taskList');
 const textInput = document.querySelector('.inputText');
+const btnClearList = document.querySelector('#btnClearList');
+const btnClearCompleted = document.querySelector('#btnClearCompleted');
+const outNumberTasks = document.querySelector('#outNumberTasks');
+const btnSortAll = document.querySelector('#btnSortAll');
+const btnSortActive = document.querySelector('#btnSortActive');
+const btnSortComplete = document.querySelector('#btnSortComplete');
 const SEQUENCE_NUMBER_ENTER = 13;
+const ACTIVE_STATUS = "active";
+const COMPLETE_STATUS = "complete"
+let taskArray = [];
 
-form.addEventListener("submit", createTask);
+(function loadToWeb() {
+    let newArr = JSON.parse(localStorage.getItem("tasks"))
+    for (let elem of newArr) {
+        createTask(elem.id, elem.status ? ACTIVE_STATUS : COMPLETE_STATUS, elem.value);
+    }
+})()
 
-function createTask(event) {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const div = document.createElement("div");
+    if (!textInput.value) return;
+    let inpText = textInput.value;
+    createTask(getToken(), ACTIVE_STATUS, inpText);
+});
+
+function getToken() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+function createTask(id, status, text) {
+    const li = document.createElement("li");
     const divText = document.createElement("div");
-    const input = document.createElement("input");
     const buttonDelete = document.createElement("button");
     const buttonEdit = document.createElement("button");
-    input.setAttribute('type', 'checkbox');
-    buttonEdit.className = 'taskButton btnEdit';
-    buttonDelete.className = 'taskButton btnDelete';
-    divText.className = 'taskText';
-    buttonEdit.textContent = 'Edit';
-    buttonDelete.textContent = 'Delete';
-    div.className = 'tasks';
-    if (!textInput.value) {
-        return;
-    }
-    divText.textContent = textInput.value;
-    textInput.value = '';
-    div.append(input);
-    div.append(divText);
-    div.append(buttonEdit);
-    div.append(buttonDelete);
-    taskList.append(div);
-    taskList.style.visibility = 'visible'
+    buttonEdit.className = "taskButton btnEdit";
+    buttonDelete.className = "taskButton btnDelete";
+    divText.className = "taskText";
+    buttonEdit.textContent = "Edit";
+    buttonDelete.textContent = "Delete";
+    li.className = "tasks";
+    li.setAttribute("data-status", status);
+    if (li.dataset.status === "complete") divText.classList.toggle("active");
+    li.setAttribute("data-id", id);
+    divText.textContent = text;
+    textInput.value = "";
+    li.append(divText);
+    li.append(buttonEdit);
+    li.append(buttonDelete);
+    taskList.append(li);
+    taskList.style.visibility = "visible";
+    taskArray.push({id: id, status: li.dataset.status === "active", value: text});
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+    outNumberTasks.textContent = `${taskArray.length}`;
+    addListener(buttonDelete, buttonEdit, divText, li);
+}
+
+function addListener(buttonDelete, buttonEdit, divText, li) {
+
     buttonDelete.addEventListener("click", () => {
-        div.remove()
+        li.remove();
+        let res = taskArray.findIndex(elem => elem.id === li.dataset.id)
+        taskArray.splice(res, 1);
+        localStorage.setItem("tasks", JSON.stringify(taskArray));
+        outNumberTasks.textContent = `${taskArray.length}`;
     });
+
     buttonEdit.addEventListener("click", () => {
-        divText.setAttribute('contenteditable', 'true');
-        divText.setAttribute('tabindex', '-1');
+        divText.setAttribute("contenteditable", "true");
+        divText.setAttribute("tabindex", "-1");
         divText.focus();
     });
 
     divText.addEventListener("blur", () => {
-        divText.setAttribute('contenteditable', 'false');
+        divText.setAttribute("contenteditable", "false")
+        taskArray.forEach(function (elem) {
+            if (elem.id === li.dataset.id) elem.value = divText.textContent;
+        })
+        localStorage.setItem("tasks", JSON.stringify(taskArray));
+        outNumberTasks.textContent = `${taskArray.length}`;
     });
 
-    divText.addEventListener('keydown', function (event) {
-        if (event.keyCode === SEQUENCE_NUMBER_ENTER) {
-            divText.blur();
+    divText.addEventListener("click", () => {
+        divText.classList.toggle("active");
+        if (li.dataset.status === "active") {
+            li.dataset.status = "complete";
+            taskArray.forEach(function (elem) {
+                if (elem.id === li.dataset.id) return elem.status = false;
+            })
+            localStorage.setItem("tasks", JSON.stringify(taskArray));
+        } else {
+            li.dataset.status = "active";
+            taskArray.forEach(function (elem) {
+                if (elem.id === li.dataset.id) return elem.status = true;
+            })
+            localStorage.setItem("tasks", JSON.stringify(taskArray));
         }
     });
-    input.addEventListener("click", function () {
-        divText.classList.toggle('active');
+
+    divText.addEventListener("keydown", function (event) {
+        if (event.keyCode === SEQUENCE_NUMBER_ENTER) divText.blur();
     });
 }
+
+btnClearList.addEventListener("click", () => {
+    taskList.innerHTML = '';
+    taskArray = [];
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+    outNumberTasks.textContent = `${taskArray.length}`;
+});
+
+btnClearCompleted.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        if (taskList.children[i].dataset.status === "complete") taskList.children[i].remove();
+        taskArray = taskArray.filter(function (elem) {
+            if (elem.status) return true
+        })
+    }
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+    console.log(taskArray.length)
+    outNumberTasks.textContent = `${taskArray.length}`;
+});
+
+btnSortAll.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        taskList.children[i].style.display = "flex"
+    }
+});
+
+btnSortActive.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        taskList.children[i].dataset.status === "active" ? taskList.children[i].style.display = "flex" : taskList.children[i].style.display = "none"
+    }
+});
+
+btnSortComplete.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        taskList.children[i].dataset.status === "complete" ? taskList.children[i].style.display = "flex" : taskList.children[i].style.display = "none"
+    }
+});
+
