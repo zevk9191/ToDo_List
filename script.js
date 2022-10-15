06a50261d0e946fc0dfb26498affa8d78a3a071c
Zakhar Bihun <12zahar34@gmail.com>
2022-10-15 17:06:27 +0300

    add and fix final features

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const form = document.body.querySelector('form');
+const form = document.querySelector('form');
 const taskList = document.querySelector('#taskList');
 const textInput = document.querySelector('.inputText');
 const btnClearList = document.querySelector('#btnClearList');
 const btnClearCompleted = document.querySelector('#btnClearCompleted');
-const selectTypeTask = document.querySelector('#selectTypeTask');
 const outNumberTasks = document.querySelector('#outNumberTasks');
+const btnSortAll = document.querySelector('#btnSortAll');
+const btnSortActive = document.querySelector('#btnSortActive');
+const btnSortComplete = document.querySelector('#btnSortComplete');
 const SEQUENCE_NUMBER_ENTER = 13;
 const ACTIVE_STATUS = "active";
 const COMPLETE_STATUS = "complete"
@@ -13,11 +15,7 @@ let taskArray = [];
 (function loadToWeb() {
     let newArr = JSON.parse(localStorage.getItem("tasks"))
     for (let elem of newArr) {
-        if (elem.status) {
-            createTask(elem.id, ACTIVE_STATUS, elem.value);
-        } else {
-            createTask(elem.id, COMPLETE_STATUS, elem.value)
-        }
+        elem.status ? createTask(elem.id, ACTIVE_STATUS, elem.value) : createTask(elem.id, COMPLETE_STATUS, elem.value);
     }
 })()
 
@@ -25,10 +23,10 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     if (!textInput.value) return;
     let inpText = textInput.value;
-    createTask(randomToken(), ACTIVE_STATUS, inpText);
+    createTask(getToken(), ACTIVE_STATUS, inpText);
 });
 
-function randomToken() {
+function getToken() {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
@@ -53,7 +51,7 @@ function createTask(id, status, text) {
     li.append(buttonDelete);
     taskList.append(li);
     taskList.style.visibility = "visible";
-    taskArray.push({id: li.dataset.id, status: li.dataset.status === "active", value: divText.textContent});
+    taskArray.push({id: id, status: li.dataset.status === "active", value: text});
     localStorage.setItem("tasks", JSON.stringify(taskArray));
     outNumberTasks.textContent = `${taskArray.length}`;
     addListener(buttonDelete, buttonEdit, divText, li);
@@ -63,9 +61,8 @@ function addListener(buttonDelete, buttonEdit, divText, li) {
 
     buttonDelete.addEventListener("click", () => {
         li.remove();
-        taskArray.forEach(function (elem, index) {
-            if (elem.id === li.dataset.id) taskArray.splice(index, 1);
-        });
+        let res = taskArray.findIndex(elem => elem.id === li.dataset.id)
+        taskArray.splice(res, 1);
         localStorage.setItem("tasks", JSON.stringify(taskArray));
         outNumberTasks.textContent = `${taskArray.length}`;
     });
@@ -79,7 +76,7 @@ function addListener(buttonDelete, buttonEdit, divText, li) {
     divText.addEventListener("blur", () => {
         divText.setAttribute("contenteditable", "false")
         taskArray.forEach(function (elem) {
-            if (elem.id === li.dataset.id) return elem.value = divText.textContent;
+            if (elem.id === li.dataset.id) elem.value = divText.textContent;
         })
         localStorage.setItem("tasks", JSON.stringify(taskArray));
         outNumberTasks.textContent = `${taskArray.length}`;
@@ -93,14 +90,12 @@ function addListener(buttonDelete, buttonEdit, divText, li) {
                 if (elem.id === li.dataset.id) return elem.status = false;
             })
             localStorage.setItem("tasks", JSON.stringify(taskArray));
-            outNumberTasks.textContent = `${taskArray.length - 1}`;
         } else {
             li.dataset.status = "active";
             taskArray.forEach(function (elem) {
                 if (elem.id === li.dataset.id) return elem.status = true;
             })
             localStorage.setItem("tasks", JSON.stringify(taskArray));
-            outNumberTasks.textContent = `${taskArray.length}`;
         }
     });
 
@@ -119,8 +114,8 @@ btnClearList.addEventListener("click", () => {
 btnClearCompleted.addEventListener("click", () => {
     for (let i = taskList.children.length - 1; i >= 0; i--) {
         if (taskList.children[i].dataset.status === "complete") taskList.children[i].remove();
-        taskArray.forEach(function (elem, index) {
-            if (!elem.status) taskArray.splice(index, 1);
+        taskArray = taskArray.filter(function (elem) {
+            if (elem.status) return true
         })
     }
     localStorage.setItem("tasks", JSON.stringify(taskArray));
@@ -128,34 +123,21 @@ btnClearCompleted.addEventListener("click", () => {
     outNumberTasks.textContent = `${taskArray.length}`;
 });
 
-selectTypeTask.addEventListener("change", () => {
-    switch (selectTypeTask.selectedIndex) {
-        case 0 :
-            for (let i = taskList.children.length - 1; i >= 0; i--) {
-                taskList.children[i].style.display = "flex"
-            }
-            break;
-        case 1 :
-            for (let i = taskList.children.length - 1; i >= 0; i--) {
-                if (taskList.children[i].dataset.status === "active") {
-                    taskList.children[i].style.display = "flex"
-                } else {
-                    taskList.children[i].style.display = "none"
-                }
-            }
-            break;
-        case 2 :
-            for (let i = taskList.children.length - 1; i >= 0; i--) {
-                if (taskList.children[i].dataset.status === "complete") {
-                    taskList.children[i].style.display = "flex"
-                } else {
-                    taskList.children[i].style.display = "none"
-                }
-            }
-            break;
-        default:
-            for (let i = taskList.children.length - 1; i >= 0; i--) {
-                taskList.children[i].style.display = "flex"
-            }
+btnSortAll.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        taskList.children[i].style.display = "flex"
     }
 });
+
+btnSortActive.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        taskList.children[i].dataset.status === "active" ? taskList.children[i].style.display = "flex" : taskList.children[i].style.display = "none"
+    }
+});
+
+btnSortComplete.addEventListener("click", () => {
+    for (let i = taskList.children.length - 1; i >= 0; i--) {
+        taskList.children[i].dataset.status === "complete" ? taskList.children[i].style.display = "flex" : taskList.children[i].style.display = "none"
+    }
+});
+
